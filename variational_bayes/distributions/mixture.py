a5d1ffa0ccff18c41640c4529f7ea546adffa9da
Till Hoffmann <tillahoffmann@gmail.com>
2017-04-26 21:07:49 +0100

    Implement likelihood of InteractingMixtureLikelihood.

--- variational_bayes/distributions/mixture.py
+++ variational_bayes/distributions/mixture.py
@@ -39,44 +39,57 @@ class MixtureLikelihood(Likelihood):
                 # We assume that the natural parameters have shape `(n, k, ...)`, where `n` is the
                 # number of observations, `k` is the number of mixture components, and `...` is the
                 # shape associated with the natural parameter
-                assert z_1.shape == value.shape[:2], "leading dimensions of the natural parameters " \
-                    "'%s' %s and the indicators %s must match" % (key, value.shape, z_1.shape)
+                assert z_1.shape == value.shape[:2], "leading dimensions %s of the natural " \
+                    "parameters %s of %s and the indicators %s must match" % \
+                    (value.shape, key, variable, z_1.shape)
                 # Sum over the dimension corresponding to samples
                 natural_parameters[key] = np.sum(pad_dims(z_1, value.ndim) * value, axis=0)
             return natural_parameters
 
 
 class InteractingMixtureLikelihood(Likelihood):
+    def __init__(self, z, likelihood, **kwargs):
+        super(InteractingMixtureLikelihood, self).__init__(z=z, likelihood=likelihood, **kwargs)
+
     @staticmethod
-    def evaluate(z, likelihood, *args):
-        pass
+    def zz(z):
+        # Compute the expected outer product
+        z_1 = s(z, 1)
+        assert z_1.ndim == 2, "indicator must be two-dimensional"
+        n, _ = z_1.shape
+        zz = z_1[:, None, :, None] * z_1[None, :, None, :]
+        i = np.arange(n)
+        zz[i, i] += s(z, 'cov')
+        return zz
+
+    @staticmethod
+    def evaluate(z, likelihood, **kwargs):
+        likelihood = likelihood.evaluate(**kwargs)
+        zz = InteractingMixtureLikelihood.zz(z)
+        # Aggregate with respect to the indicator dimensions
+        return np.sum(pad_dims(zz, likelihood.ndim) * likelihood, axis=(2, 3))
 
     @staticmethod
     def natural_parameters(variable, z, likelihood, **kwargs):
         if variable == 'z':
             raise NotImplementedError("Natural parameters for indicators cannot be obtained jointly")
         else:
-            # Compute the expected outer product
-            z_1 = s(z, 1)
-            assert z_1.ndim == 2, "indicator must be two-dimensional"
-            n, _ = z_1.shape
-            zz = z_1[:, None, :, None] * z_1[None, :, None, :]
-            i = np.arange(n)
-            zz[i, i] += s(z, 'cov')
+            zz = InteractingMixtureLikelihood.zz(z)
             # Iterate over the natural parameters of the likelihood
             natural_parameters = {}
             for key, value in likelihood.natural_parameters(variable, **kwargs).items():
                 # We assume that the natural parameters have shape `(n, n, k, k, ...)` where `n` is
                 # the number of observations, `k` is the number of mixture components, and `...` is
                 # the shape associated with the natural parameter
-                assert zz.shape == value.shape[:4], "leading dimensions of the natural parameters " \
-                    "'%s' %s and the indicators %s must match" % (key, value.shape, zz.shape)
+                assert zz.shape == value.shape[:4], "leading dimensions %s of the natural " \
+                    "parameters %s of %s and the indicators %s must match" % \
+                    (value.shape, key, variable, zz.shape)
                 # Sum over the dimensions corresponding to samples
                 natural_parameters[key] = np.sum(pad_dims(zz, value.ndim) * value, axis=(0, 1))
             return natural_parameters
 
     @staticmethod
-    def update_indicators(z, natural_parameters, likelihood, **kwargs):
+    def indicator_natural_parameters(natural_parameters, z, likelihood, **kwargs):
         """
         Update the indicator variables for component membership.
 
@@ -102,9 +115,12 @@ class InteractingMixtureLikelihood(Likelihood):
 
         # Unpack the natural parameter associated with the mean because it is the only relevant one
         natural_parameters = natural_parameters['mean']
-        assert natural_parameters.shape == proba.shape, "shape %s of the natural parameter " \
-            "associated with the mean must match the shape %s of the expected indicators" % \
-            (natural_parameters.shape, proba.shape)
+        # Ensure the shape is broadcast
+        natural_parameters = np.ones(np.broadcast(natural_parameters, proba).shape) * natural_parameters
+        #ndim = np.ndim(natural_parameters)
+        #assert np.shape(natural_parameters) == np.shape(proba)[:-ndim], "trailing shape %s of the natural " \
+        #    "parameter associated with the mean must match the shape %s of the expected indicators" % \
+        #    (np.shape(natural_parameters), np.shape(proba))
 
         # Evaluate the likelihood
         likelihood = likelihood.evaluate(**kwargs)
@@ -128,4 +144,6 @@ class InteractingMixtureLikelihood(Likelihood):
             # Set the probability of the observations
             proba[i] = softmax(_np)
 
-        return CategoricalDistribution(proba)
+        return {
+            'mean': np.log(proba)
+        }
