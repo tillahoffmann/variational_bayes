97a28ad8fcd9709fb8404ba524e8688770438dea
Till Hoffmann <tillahoffmann@gmail.com>
2017-04-26 10:47:49 +0100

    Add interacting mixture likelihood.

--- variational_bayes/distributions/mixture.py
+++ variational_bayes/distributions/mixture.py
@@ -1,7 +1,9 @@
+import numbers
 import numpy as np
 
 from .util import s, Likelihood
-from ..util import pad_dims, sum_trailing_dims
+from .categorical import CategoricalDistribution
+from ..util import pad_dims, sum_trailing_dims, softmax
 
 
 class MixtureLikelihood(Likelihood):
@@ -27,13 +29,100 @@ class MixtureLikelihood(Likelihood):
                 'mean': sum_trailing_dims(likelihood, 2),
             }
         else:
-            # Evaluate the statistics of the likelihood and sum up over the observations
             z_1 = s(z, 1)
             assert z_1.ndim == 2, "indicator must be two-dimensional"
+            # Iterate over the natural parameters of the likelihood
             natural_parameters = {}
             for key, value in likelihood.natural_parameters(variable, *args).items():
-                # Ensure the leading dimensions of the coefficients and the indicators match
-                assert z_1.shape == value.shape[:2], "leading dimensions of the coefficients " \
+                # We assume that the natural parameters have shape `(n, k, ...)`, where `n` is the
+                # number of observations, `k` is the number of mixture components, and `...` is the
+                # shape associated with the natural parameter
+                assert z_1.shape == value.shape[:2], "leading dimensions of the natural parameters " \
                     "'%s' %s and the indicators %s must match" % (key, value.shape, z_1.shape)
-                natural_parameters[key] = np.sum(pad_dims(z_1, value.ndim) * value, axis=0)
+                # Sum over the indicator dimension
+                natural_parameters[key] = np.sum(pad_dims(z_1, value.ndim) * value, axis=1)
             return natural_parameters
+
+
+class InteractingMixtureLikelihood(Likelihood):
+    @staticmethod
+    def evaluate(z, likelihood, *args):
+        pass
+
+    @staticmethod
+    def natural_parameters(variable, z, likelihood, *args):
+        if variable == 'z':
+            raise NotImplementedError("Natural parameters for indicators cannot be obtained jointly")
+        else:
+            # Compute the expected outer product
+            z_1 = s(z, 1)
+            assert z_1.ndim == 2, "indicator must be two-dimensional"
+            n, _ = z_1.shape
+            zz = z_1[:, None, :, None] * z_1[None, :, None, :]
+            i = np.arange(n)
+            zz[i, i] += s(z, 'cov')
+            # Iterate over the natural parameters of the likelihood
+            natural_parameters = {}
+            for key, value in likelihood.natural_parameters(variable, *args).items():
+                # We assume that the natural parameters have shape `(n, n, k, k, ...)` where `n` is
+                # the number of observations, `k` is the number of mixture components, and `...` is
+                # the shape associated with the natural parameter
+                assert zz.shape == value.shape[:4], "leading dimensions of the natural parameters " \
+                    "'%s' %s and the indicators %s must match" % (key, value.shape, zz.shape)
+                # Sum over the indicator dimensions
+                natural_parameters[key] = np.sum(pad_dims(zz, value.ndim) * value, axis=(2, 3))
+            return natural_parameters
+
+    @staticmethod
+    def update_indicators(z, natural_parameters, likelihood, *args):
+        """
+        Update the indicator variables for component membership.
+
+        The natural parameters of the indicators cannot be obtained jointly and we need to run an
+        iterative update scheme in which each indicator variable is updated in turn.
+
+        Parameters
+        ----------
+        z : np.ndarray or CategoricalDistribution
+            indicator variables for component membership
+        natural_parameters : dict
+            natural parameters or messages sent to the indicator variables from the rest of the
+            Bayesian network
+        likelihood : Likelihood
+            likelihood to evaluate the likelihood of `args`
+        args : list
+            parameters passed to the evaluation of `likelihood`
+        """
+        # Start with the initial probabilities
+        proba = s(z, 1)
+        assert proba.ndim == 2, "indicator must be two-dimensional"
+        n, k = proba.shape
+
+        # Unpack the natural parameter associated with the mean because it is the only relevant one
+        natural_parameters = natural_parameters['mean']
+        assert natural_parameters.shape == proba.shape, "shape %s of the natural parameter " \
+            "associated with the mean must match the shape %s of the expected indicators" % \
+            (natural_parameters.shape, proba.shape)
+
+        # Evaluate the likelihood
+        likelihood = likelihood.evaluate(*args)
+        assert likelihood.ndim >= 4, "evaluated likelihood must have at least four dimensions"
+        assert likelihood.shape[:4] == (n, n, k, k), "evaluated likelihood must have leading shape " \
+            "%s" % [(n, n, k, k)]
+        # Collapse the trailing dimensions
+        likelihood = sum_trailing_dims(likelihood, 4)
+
+        # Iterate over the nodes
+        for i in range(n):
+            # Initialize the natural parameters for this observation
+            _np = natural_parameters[i]
+            # Add the diagonal term
+            _np += np.diag(likelihood[i, i])
+            # Add the interaction terms
+            _np += np.einsum('jb,jab', proba, likelihood[i])
+            # Subtract the self interaction (we shouldn't have added this term in the first place)
+            _np -= np.sum(likelihood[i, i], axis=1)
+            # Set the probability of the node
+            proba[i] = softmax(_np)
+
+        return CategoricalDistribution(proba)
