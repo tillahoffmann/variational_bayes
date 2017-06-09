08885fc1159ba6857f922762acb45d443b91db6a
Till Hoffmann <tillahoffmann@gmail.com>
2017-06-09 14:05:33 +0100

    Remove IndexedDistribution.

--- variational_bayes/distributions/__init__.py
+++ variational_bayes/distributions/__init__.py
@@ -1,10 +1,10 @@
+# pylint: disable=W0401
 from .bernoulli import *
 from .beta import *
 from .categorical import *
 from .dirichlet import *
 from .distribution import *
 from .gamma import *
-from .indexed import *
 from .mixture import *
 from .multinormal import *
 from .normal import *
