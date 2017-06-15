d3705f4be194501b1f1ff97a0e02934458ec60de
Till Hoffmann <tillahoffmann@gmail.com>
2017-06-15 16:50:11 +0100

    Add uniform initial conditions.

--- variational_bayes/models/var.py
+++ variational_bayes/models/var.py
@@ -7,7 +7,7 @@ from ..distributions import NormalDistribution, GammaDistribution, MultiNormalDi
 from .interacting_mixture_model import InteractingMixtureModel
 
 
-def var_model(x, order, num_groups, update_order=None, given=None):
+def var_model(x, order, num_groups, update_order=None, given=None, uniform_ic=False):
     """
     Build a hierarchical vector-autoregressive model.
 
@@ -18,40 +18,75 @@ def var_model(x, order, num_groups, update_order=None, given=None):
 
     epsilon = 1e-6
 
-    factors = {
-        'coefficients': MultiNormalDistribution(
-            np.random.normal(0, epsilon, (num_nodes, order * num_nodes + 1)),
-            np.ones((num_nodes, 1, 1)) * np.eye(order * num_nodes + 1) * epsilon
-        ),
-        'density': DirichletDistribution(
-            np.random.uniform(1 - epsilon, 1 + epsilon, num_groups)
-        ),
-        'z': CategoricalDistribution(
-            np.random.dirichlet(np.ones(num_groups) / epsilon, num_nodes),
-        ),
-        'bias_mean': NormalDistribution(
-            np.random.normal(0, epsilon, num_groups),
-            np.ones(num_groups) * epsilon,
-        ),
-        'bias_precision': GammaDistribution(
-            1e-3 + np.random.normal(0, epsilon, num_groups),
-            1e-3 * np.ones(num_groups),
-        ),
-        'adjacency_mean': MultiNormalDistribution(
-            np.random.normal(0, epsilon, (num_groups, num_groups, order)),
-            np.ones((num_groups, num_groups, 1, 1)) * np.eye(order) * epsilon
-        ),
-        'adjacency_precision': WishartDistribution(
-            order - 1 + 1e-3 + np.random.normal(0, epsilon, (num_groups, num_groups)),
-            (order - 1 + 1e-3) * np.ones((num_groups, num_groups, 1, 1)) * np.eye(order)
-        ),
-        'noise_precision': GammaDistribution(
-            1e-3 + np.random.normal(0, epsilon, num_groups),
-            1e-3 * np.ones(num_groups),
-        )
-    }
-
-    factors.update(given or {})
+    if uniform_ic:
+        factors = {
+            'coefficients': MultiNormalDistribution(
+                np.zeros((num_nodes, order * num_nodes + 1)),
+                np.ones((num_nodes, 1, 1)) * np.eye(order * num_nodes + 1) * epsilon
+            ),
+            'density': DirichletDistribution(
+                np.ones(num_groups)
+            ),
+            'bias_mean': NormalDistribution(
+                np.ones(num_groups),
+                np.ones(num_groups) * epsilon,
+            ),
+            'bias_precision': GammaDistribution(
+                epsilon * np.ones(num_groups),
+                epsilon * np.ones(num_groups),
+            ),
+            'adjacency_mean': MultiNormalDistribution(
+                np.zeros((num_groups, num_groups, order)),
+                np.ones((num_groups, num_groups, 1, 1)) * np.eye(order) * epsilon
+            ),
+            'adjacency_precision': WishartDistribution(
+                order * np.ones((num_groups, num_groups)),
+                order * np.ones((num_groups, num_groups, 1, 1)) * np.eye(order)
+            ),
+            'noise_precision': GammaDistribution(
+                1e-3 + np.random.normal(0, epsilon, num_groups),
+                1e-3 * np.ones(num_groups),
+            ),
+            # Only random IC to break symmetry
+            'z': CategoricalDistribution(
+                np.random.dirichlet(np.ones(num_groups) / epsilon, num_nodes),
+            ),
+        }
+    else:
+        factors = {
+            'coefficients': MultiNormalDistribution(
+                np.random.normal(0, epsilon, (num_nodes, order * num_nodes + 1)),
+                np.ones((num_nodes, 1, 1)) * np.eye(order * num_nodes + 1) * epsilon
+            ),
+            'density': DirichletDistribution(
+                np.random.uniform(1 - epsilon, 1 + epsilon, num_groups)
+            ),
+            'z': CategoricalDistribution(
+                np.random.dirichlet(np.ones(num_groups) / epsilon, num_nodes),
+            ),
+            'bias_mean': NormalDistribution(
+                np.random.normal(0, epsilon, num_groups),
+                np.ones(num_groups) * epsilon,
+            ),
+            'bias_precision': GammaDistribution(
+                1e-3 + np.random.normal(0, epsilon, num_groups),
+                1e-3 * np.ones(num_groups),
+            ),
+            'adjacency_mean': MultiNormalDistribution(
+                np.random.normal(0, epsilon, (num_groups, num_groups, order)),
+                np.ones((num_groups, num_groups, 1, 1)) * np.eye(order) * epsilon
+            ),
+            'adjacency_precision': WishartDistribution(
+                order - 1 + 1e-3 + np.random.normal(0, epsilon, (num_groups, num_groups)),
+                (order - 1 + 1e-3) * np.ones((num_groups, num_groups, 1, 1)) * np.eye(order)
+            ),
+            'noise_precision': GammaDistribution(
+                1e-3 + np.random.normal(0, epsilon, num_groups),
+                1e-3 * np.ones(num_groups),
+            )
+        }
+    given = given or {}
+    factors.update(given)
 
     # Extract the factors
     q_coefficients = factors['coefficients']
@@ -89,10 +124,19 @@ def var_model(x, order, num_groups, update_order=None, given=None):
         WishartDistribution(order - 1 + 1e-6, np.eye(order) * 1e-6).likelihood(q_adjacency_precision)
     ]
 
-    update_order = update_order or [
-        "coefficients", "adjacency_mean", "adjacency_precision", "bias_mean", "bias_precision",
-        "noise_precision", "z", "density"
-    ]
+    if uniform_ic:
+        update_order = update_order or [
+            "coefficients", "noise_precision", "bias_mean", "bias_precision", "adjacency_mean",
+            "adjacency_precision", "z", "density"
+        ]
+    else:
+        update_order = update_order or [
+            "coefficients", "adjacency_mean", "adjacency_precision", "bias_mean", "bias_precision",
+            "noise_precision", "z", "density"
+        ]
+
+    for key in given:
+        update_order.remove(key)
 
     factors = {key: value for key, value in factors.items() if isinstance(value, Distribution)}
     model = InteractingMixtureModel(factors, likelihoods, update_order)
